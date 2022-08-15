0da2d8e805bd286a749ff7beccee66bd5294bc3a
Aman Shukla <aman56.champ@gmail.com>
2022-08-15 14:59:00 -0400

    Updated check functionality with path

--- examples/random_model.py
+++ examples/random_model.py
@@ -1,10 +1,11 @@
-from label_data import IMAGE_PATH, check_image_exists
+from importlib.resources import path
+from label_data import check_image_exists
 from pandas import DataFrame, read_csv, concat
 
-def return_valid_frame(dataframe):
+def return_valid_frame(dataframe, path):
         valid = DataFrame()
         for _, row in dataframe.iterrows():
-                if check_image_exists(row.image_id):
+                if check_image_exists(row.image_id, path):
                         valid = valid.append(row)
         return valid
 
