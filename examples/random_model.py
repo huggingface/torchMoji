f1fd1bac7bef6c2417b7fbdbeac3f0c6ea190cbc
Aman Shukla <aman56.champ@gmail.com>
2022-09-22 17:47:49 -0400

    Label Mismatch

--- examples/random_model.py
+++ examples/random_model.py
@@ -1,12 +1,28 @@
-from importlib.resources import path
-from label_data import check_image_exists
+# from label_data import check_image_exists
 from pandas import DataFrame, read_csv, concat
+import glob
+
+def check_image_exists(unique_image_id,path):
+
+    if glob.glob(path + f"/{unique_image_id}.png", recursive=True):
+        return True
+    
+    return False
+
 
 def return_valid_frame(dataframe, path):
         valid = DataFrame()
         for _, row in dataframe.iterrows():
                 if check_image_exists(row.image_id, path):
                         valid = valid.append(row)
+
+        # valid['2'] = 0
+        valid['11'] = 0
+        valid['48'] = 0
+
+        new_cols = ['image_id', 'emoji']+[str(i) for i in range(64)]
+        valid = valid[new_cols]
+
         return valid
 
 # if __name__ == '__main__':
